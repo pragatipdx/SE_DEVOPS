2d2e6a71ccbac3da4692e51f60f11944a53f53ff
pragatipdx <pragati@pdx.edu>
2021-11-16 15:46:55 -0800

    Java main and test class

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String args[]) {
+        System.out.println("Program Ran Successfully :  " + isGreater(4, 3));
+
+    }
+
+    public static boolean isGreater(int a, int b) {
+        if (a > b)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+    App test=new App();
+
+
+    @Test
+    public void checkString()
+    { assertTrue(test.isGreater(6,5));
+
+
+    }
+}
